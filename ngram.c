473cddc83c69f093ab1cb59273d58d3b7758c8de
DoodleBin <alexahatcher4@gmail.com>
2026-09-10 16:12:21 -0500

    Added Ngram sliding window code

--- ngram.c
+++ ngram.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 int main(int argc, char** argv) {
   // Make sure the user provided an N parameter on the command line
@@ -19,6 +20,37 @@ int main(int argc, char** argv) {
   }
 
   // TODO: read from standard input and print out ngrams until you reach the end of the input
+  
+  int c;
+  int stringIndex = 0;
+  int currentWindow[N];
+  bool hasPrinted = false;
+
+  while ((c = fgetc(stdin)) != EOF) {
+    int currIndex = stringIndex % N;
+    currentWindow[currIndex] = c;
+
+   // If we filled the first window, print it
+    if (stringIndex >= N - 1) {
+
+      //loop our currentWindow array and print the window
+      for (int i = 0; i < N; i++) {
+
+        // Subtract N from how many chars we've seen so far and add 1 
+        // --> finds start of our window
+        int idx = (stringIndex - N + 1 + i) % N;
+        printf("%c", currentWindow[idx]);
+      }
+      printf("\n");
+      hasPrinted = true;
+    }
+    
+    stringIndex++;
+  }
+
+  if (hasPrinted == false) {
+    return 0;
+  }
 
   return 0;
 }
